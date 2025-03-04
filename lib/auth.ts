8a0815eeb798fc218b207d1a7bb03c8bca64c5ce
Themba <mndebele.themba@gmail.com>
2025-03-04 20:05:49 +0200

    Add authentication features with one-tap sign-in and magic link; update dependencies and UI components

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,25 @@
 import { betterAuth } from "better-auth";
+import { VercelPool } from "@vercel/postgres";
+
+import { magicLink, oneTap } from "better-auth/plugins";
 
 export const auth = betterAuth({
-  //...
+  database: new VercelPool({
+    connectionString: process.env.POSTGRES_URL as string,
+  }),
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // Cache duration in seconds
+    },
+  },
+  plugins: [
+    oneTap(),
+    magicLink({
+      sendMagicLink: async ({ email, token, url }, request) => {
+        // send email to user
+        console.log(email, token, url, request);
+      },
+    }),
+  ],
 });
